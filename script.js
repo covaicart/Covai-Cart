43fa3c71a06f58507830dfa6a193b065789f964f
covaicart <nngamingytofficial@gmail.com>
2025-04-13 16:53:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function addToCart(name, price) {
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
-  cart.push({ name, price });
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert(`${name} added to cart!`);
-}
